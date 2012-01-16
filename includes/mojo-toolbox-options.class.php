b72b8c6fdd9ab8d146e58dd825da0c1a4cbd7b4e
mojowill <mojowill@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-16 17:15:48 +0000

    Tidying up the admin page.
    git-svn-id: http://plugins.svn.wordpress.org/the-mojo-admin-toolbox/trunk@490669 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/mojo-toolbox-options.class.php
+++ includes/mojo-toolbox-options.class.php
@@ -94,7 +94,6 @@ if ( ! class_exists( 'mojoToolboxOptions' ) ) :
 		 * @access public
 		 * @return void
 		 * @since 1.0
-		 * @todo TIDY UP!
 		 */
 		function render_form() {
 		
@@ -117,68 +116,68 @@ if ( ! class_exists( 'mojoToolboxOptions' ) ) :
 						<tr valign="top">
 							<th scope="row"><?php echo _e( 'General Options', 'mojo-toolbox' );?></th>
 							<td>
-								<label><input name="mojoToolbox_options[remove_comments]" type="checkbox" value="1" <?php if (isset($this->options['remove_comments'])) { checked('1', $this->options['remove_comments']); } ?> /> <?php echo _e( 'Hide the comments system.', 'mojo-toolbox' );?></label><br />
-								<label><input name="mojoToolbox_options[remove_editor]" type="checkbox" value="1" <?php if (isset($this->options['remove_editor'])) { checked('1', $this->options['remove_editor']); } ?> /> <?php echo _e( 'Hide the theme editor screen.', 'mojo-toolbox' );?></label><br />
-								<label><input name="mojoToolbox_options[hide_wp]" type="checkbox" value="1" <?php if (isset($this->options['hide_wp'])) { checked('1', $this->options['hide_wp']); } ?> /> <?php echo _e( 'Hide the Wordpress Meta Generator tag.', 'mojo-toolbox' );?></label><br />
+								<label><input name="mojoToolbox_options[remove_comments]" type="checkbox" value="1" <?php if ( isset( $this->options['remove_comments'] ) ) { checked( '1', $this->options['remove_comments'] ); } ?> /> <?php echo _e( 'Hide the comments system.', 'mojo-toolbox' );?></label><br />
+								<label><input name="mojoToolbox_options[remove_editor]" type="checkbox" value="1" <?php if ( isset( $this->options['remove_editor'] ) ) { checked( '1', $this->options['remove_editor'] ); } ?> /> <?php echo _e( 'Hide the theme editor screen.', 'mojo-toolbox' );?></label><br />
+								<label><input name="mojoToolbox_options[hide_wp]" type="checkbox" value="1" <?php if ( isset( $this->options['hide_wp'] ) ) { checked( '1', $this->options['hide_wp'] ); } ?> /> <?php echo _e( 'Hide the Wordpress Meta Generator tag.', 'mojo-toolbox' );?></label><br />
 							</td>
 						</tr>
 						
 						<tr valign="top">
 							<th scope="row"><?php echo _e( 'Dashboard Options', 'mojo-toolbox' );?></th>
 							<td>
-								<label><input name="mojoToolbox_options[dashboard_plugins]" type="checkbox" value="1" <?php if (isset($this->options['dashboard_plugins'])) { checked('1', $this->options['dashboard_plugins']); } ?> /> <?php echo _e( 'Hide the "Plugins" dashboard widget.', 'mojo-toolbox' );?></label><br />
-								<label><input name="mojoToolbox_options[dashboard_primary]" type="checkbox" value="1" <?php if (isset($this->options['dashboard_primary'])) { checked('1', $this->options['dashboard_primary']); } ?> /> <?php echo _e( 'Hide the "Wordpress Blog" dashboard widget.', 'mojo-toolbox' );?></label><br />
-								<label><input name="mojoToolbox_options[dashboard_links]" type="checkbox" value="1" <?php if (isset($this->options['dashboard_links'])) { checked('1', $this->options['dashboard_links']); } ?> /> <?php echo _e( 'Hide the "Incoming links" dashboard widget.', 'mojo-toolbox' );?></label><br />
-								<label><input name="mojoToolbox_options[dashboard_secondary]" type="checkbox" value="1" <?php if (isset($this->options['dashboard_secondary'])) { checked('1', $this->options['dashboard_secondary']); } ?> /> <?php echo _e( 'Hide the "Other News" dashboard widget.', 'mojo-toolbox' );?></label><br />
+								<label><input name="mojoToolbox_options[dashboard_plugins]" type="checkbox" value="1" <?php if ( isset( $this->options['dashboard_plugins'] ) ) { checked( '1', $this->options['dashboard_plugins'] ); } ?> /> <?php echo _e( 'Hide the "Plugins" dashboard widget.', 'mojo-toolbox' );?></label><br />
+								<label><input name="mojoToolbox_options[dashboard_primary]" type="checkbox" value="1" <?php if ( isset( $this->options['dashboard_primary'] ) ) { checked( '1', $this->options['dashboard_primary'] ); } ?> /> <?php echo _e( 'Hide the "Wordpress Blog" dashboard widget.', 'mojo-toolbox' );?></label><br />
+								<label><input name="mojoToolbox_options[dashboard_links]" type="checkbox" value="1" <?php if ( isset( $this->options['dashboard_links'] ) ) { checked( '1', $this->options['dashboard_links'] ); } ?> /> <?php echo _e( 'Hide the "Incoming links" dashboard widget.', 'mojo-toolbox' );?></label><br />
+								<label><input name="mojoToolbox_options[dashboard_secondary]" type="checkbox" value="1" <?php if ( isset( $this->options['dashboard_secondary'] ) ) { checked( '1', $this->options['dashboard_secondary'] ); } ?> /> <?php echo _e( 'Hide the "Other News" dashboard widget.', 'mojo-toolbox' );?></label><br />
 							</td>
 						</tr>
 
 						<tr>
 							<th scope="row"><?php echo _e( 'Custom Login Logo URL', 'mojo-toolbox' );?></th>
 							<td>
-								<label><input name="mojoToolbox_options[login_logo]" type="text" value="<?php if (isset($this->options['login_logo'])) echo $this->options['login_logo'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The URL for your custom login page logo, (270px x 60px works well!)', 'mojo-toolbox' );?></span></label>
+								<label><input name="mojoToolbox_options[login_logo]" type="text" value="<?php if ( isset( $this->options['login_logo'] ) ) echo $this->options['login_logo'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The URL for your custom login page logo, (270px x 60px works well!)', 'mojo-toolbox' );?></span></label>
 							</td>
 						</tr>
 						
 						<tr>
 							<th scope="row"><?php echo _e( 'Custom Login URL', 'mojo-toolbox' );?></th>
 							<td>
-								<label><input name="mojoToolbox_options[login_url]" type="text" value="<?php if (isset($this->options['login_url'])) echo $this->options['login_url'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The URL to goto when clicking on the login screen logo. (defaults to the site url when activated, instead of wordpress.org)', 'mojo-toolbox' );?></span></label>
+								<label><input name="mojoToolbox_options[login_url]" type="text" value="<?php if ( isset( $this->options['login_url'] ) ) echo $this->options['login_url'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The URL to goto when clicking on the login screen logo. (defaults to the site url when activated, instead of wordpress.org)', 'mojo-toolbox' );?></span></label>
 							</td>
 						</tr>
 						
 						<tr>
 							<th scope="row"><?php echo _e( 'Custom Login Description', 'mojo-toolbox' );?></th>
 							<td>
-								<label><input name="mojoToolbox_options[login_desc]" type="text" value="<?php if (isset($this->options['login_desc'])) echo $this->options['login_desc'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The text to show as the page title and hover text on the login screen. (defaults to the Blog Description when activated)', 'mojo-toolbox' );?></span></label>
+								<label><input name="mojoToolbox_options[login_desc]" type="text" value="<?php if ( isset( $this->options['login_desc'] ) ) echo $this->options['login_desc'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The text to show as the page title and hover text on the login screen. (defaults to the Blog Description when activated)', 'mojo-toolbox' );?></span></label>
 							</td>
 						</tr>
 						
 						<tr>
 							<th scope="row"><?php echo _e( 'Custom Admin Logo URL', 'mojo-toolbox' );?></th>
 							<td>
-								<label><input name="mojoToolbox_options[admin_logo]" type="text" value="<?php if (isset($this->options['admin_logo'])) echo $this->options['admin_logo'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The URL for your custom login page logo, (16px x 16px!)', 'mojo-toolbox' );?></span></label>
+								<label><input name="mojoToolbox_options[admin_logo]" type="text" value="<?php if ( isset( $this->options['admin_logo'] ) ) echo $this->options['admin_logo'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The URL for your custom login page logo, (16px x 16px!)', 'mojo-toolbox' );?></span></label>
 							</td>
 						</tr>
 						
 						<tr>
 							<th scope="row"><?php echo _e( 'Custom Avatar', 'mojo-toolbox' );?></th>
 							<td>
-								<label><input name="mojoToolbox_options[avatar_image]" type="text" value="<?php if (isset($this->options['avatar_image'])) echo $this->options['avatar_image'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The URL for your custom avatar, (70px x 70px!)', 'mojo-toolbox' );?></span></label>
+								<label><input name="mojoToolbox_options[avatar_image]" type="text" value="<?php if ( isset( $this->options['avatar_image'] ) ) echo $this->options['avatar_image'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The URL for your custom avatar, (70px x 70px!)', 'mojo-toolbox' );?></span></label>
 							</td>
 						</tr>
 						
 						<tr>
 							<th scope="row"><?php echo _e( 'Custom Avatar Title', 'mojo-toolbox' );?></th>
 							<td>
-								<label><input name="mojoToolbox_options[avatar_title]" type="text" value="<?php if (isset($this->options['avatar_title'])) echo $this->options['avatar_title'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The title for your custom avatar', 'mojo-toolbox' );?></span></label>
+								<label><input name="mojoToolbox_options[avatar_title]" type="text" value="<?php if ( isset( $this->options['avatar_title'] ) ) echo $this->options['avatar_title'];?>"/><span style="color:#666666;margin-left:2px;"><?php echo _e( 'The title for your custom avatar', 'mojo-toolbox' );?></span></label>
 							</td>
 						</tr>
 						
 						<tr>
 							<th scope="row"><?php echo _e( 'Dashboard Footer Text', 'mojo-toolbox' );?></th>
 							<td>
-								<textarea name="mojoToolbox_options[footer_text]" rows="7" cols="50"><?php if (isset($this->options['footer_text'])) echo $this->options['footer_text'];?></textarea><br /><span style="color:#666666; margin-left: 2px;"><?php echo _e( 'Text to show in the dashboard footer area', 'mojo-toolbox' );?></span>
+								<textarea name="mojoToolbox_options[footer_text]" rows="7" cols="50"><?php if ( isset( $this->options['footer_text'] ) ) echo $this->options['footer_text'];?></textarea><br /><span style="color:#666666; margin-left: 2px;"><?php echo _e( 'Text to show in the dashboard footer area', 'mojo-toolbox' );?></span>
 							</td>
 						</tr>
 		
@@ -186,13 +185,13 @@ if ( ! class_exists( 'mojoToolboxOptions' ) ) :
 						<tr valign="top" style="border-top:#dddddd 1px solid;">
 							<th scope="row"><?php echo _e( 'Database Options', 'mojo-toolbox');?></th>
 							<td>
-								<label><input name="mojoToolbox_options[chk_default_options_db]" type="checkbox" value="1" <?php if (isset($this->options['chk_default_options_db'])) { checked('1', $this->options['chk_default_options_db']); } ?> /> <?php echo _e( 'Restore defaults upon plugin deactivation/reactivation', 'mojo-toolbox');?></label>
-								<br /><span style="color:red;margin-left:2px;"><?php echo _e( 'Only check this if you want to reset plugin settings upon Plugin reactivation', 'mojo-toolbox');?></span>
+								<label><input name="mojoToolbox_options[chk_default_options_db]" type="checkbox" value="1" <?php if ( isset( $this->options['chk_default_options_db'] ) ) { checked( '1', $this->options['chk_default_options_db']); } ?> /> <?php echo _e( 'Restore defaults upon plugin deactivation/reactivation', 'mojo-toolbox' );?></label>
+								<br /><span style="color:red;margin-left:2px;"><?php echo _e( 'Only check this if you want to reset plugin settings upon Plugin reactivation', 'mojo-toolbox' );?></span>
 							</td>
 						</tr>
 					</table>
 					<p class="submit">
-					<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+					<input type="submit" class="button-primary" value="<?php _e( 'Save Changes', 'mojo-toolbox' ); ?>" />
 					</p>
 				</form>
 			</div>
